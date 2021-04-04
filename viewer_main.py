2cb4ca8d7e770a35026296bc614a8346d3bceabb
Thomas Bouvier <thomas.bouvier@epitech.eu>
2021-04-04 19:03:48 -0400

    Small improvements : images are smaller and can be viewed in higher resolution by clicking on them + add video support.

--- viewer_main.py
+++ viewer_main.py
@@ -8,7 +8,10 @@ app = Flask(__name__)
 def list_images(profile):
     if profile not in os.listdir(app.static_folder):
         return redirect("/", code=302)
-    return render_template("gallery.html", profile=profile, images=os.listdir(app.static_folder + "/" + profile))
+    files = os.listdir(app.static_folder + "/" + profile)
+    images = [f for f in files if f.lower().endswith(".jpg") or f.lower().endswith(".png") or f.lower().endswith("jpeg")]
+    videos = [f for f in files if f.lower().endswith(".mp4")]
+    return render_template("gallery.html", profile=profile, images=images, videos=videos)
 
 
 @app.route("/")
