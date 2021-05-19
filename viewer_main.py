887012b414bea5061b8e8318fa0835d2077b5ece
Thomas Bouvier <thomas.bouvier@epitech.eu>
2021-05-19 17:01:07 +0200

    Small upgrades to pictures viewer.

--- viewer_main.py
+++ viewer_main.py
@@ -10,9 +10,15 @@ def list_images(profile):
     if profile not in os.listdir(app.static_folder):
         return redirect("/", code=302)
     files = os.listdir(app.static_folder + "/" + profile)
-    images = [f for f in files if f.lower().endswith(".jpg") or f.lower().endswith(".png") or f.lower().endswith("jpeg")]
+    ii = [f for f in files if f.lower().endswith(".jpg") or f.lower().endswith(".png") or f.lower().endswith("jpeg")]
     videos = [f for f in files if f.lower().endswith(".mp4")]
-    return render_template("gallery.html", profile=profile, images=images, videos=videos)
+
+    images = []
+    for i in ii:
+        with open(app.static_folder + "/" + profile + "/" + i, "rb") as f:
+            images.append({"name": i, "data": base64.b64encode(f.read()).decode("ascii")})
+
+    return render_template("gallery.html", root="static", profile=profile, images=images, videos=videos)
 
 
 @app.route("/")
