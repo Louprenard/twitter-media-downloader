520aa9d80d5737fe18595a548db54759d6301359
Thomas Bouvier <thomas.bouvier@epitech.eu>
2021-07-07 19:59:01 +0200

    Remove viewer (not very stable and very slow), improved a bit the downloader by splitting the main parts into functions.

--- downloader_main.py
+++ downloader_main.py
@@ -8,13 +8,13 @@ import youtube_dl
 import sys
 from pathlib import Path
 
-VERBOSE = False
+VERBOSE = True
 
 if "-v" in sys.argv:
     VERBOSE = True
 
 try:
-    Path("static").mkdir(exist_ok=False)
+    Path("images").mkdir(exist_ok=False)
 except FileExistsError: pass
 
 try:
@@ -27,106 +27,120 @@ try:
         f.write("{}")
 except FileExistsError: pass
 
-# users.txt : contains twitter usernames, one per line.
-users = []
-with open("users.txt", encoding="utf-8") as f:
-    for line in f.read().splitlines():
-        if line.startswith("# "): continue
-        users.append(line)
 
 
-with open("config.json", encoding="utf-8") as f:
-    data = json.load(f)
+def create_cache():
+    users = []
+    # users.txt : contains twitter usernames, one per line.
+    with open("users.txt", encoding="utf-8") as f:
+        for line in f.read().splitlines():
+            if line.startswith("# "): continue
+            users.append(line)
 
-tweets = []
 
-for user in users:
-    try:
-        Path("./static/" + user).mkdir(exist_ok=False)
-    except FileExistsError: pass
-    print(user)
+    with open("config.json", encoding="utf-8") as f:
+        data = json.load(f)
 
-    tconfig = twint.Config()
+    tweets = []
 
-    tconfig.Hide_output = not VERBOSE
-    tconfig.Username = user
-    # If only static
-    # tconfig.Images = True
-    # If only videos
-    # tconfig.Videos = True
-    tconfig.Media = True
-    tconfig.Store_csv = True
-    tconfig.Output = "cache_" + user + ".csv"
-    if user in data and data[user]:
-        tconfig.Since = data[user]
+    for user in users:
+        try:
+            Path("./images/" + user).mkdir(exist_ok=False)
+        except FileExistsError: pass
+        print(user)
 
-    try:
-        twint.run.Search(tconfig)
-    except Exception as e:
-        # print(e)
-        print("Issue with Twitter scrapping, retry later")
+        tconfig = twint.Config()
 
-    try: 
+        tconfig.Hide_output = not VERBOSE
+        tconfig.Username = user
+        # If only static
+        # tconfig.Images = True
+        # If only videos
+        # tconfig.Videos = True
+        tconfig.Media = True
+        tconfig.Store_csv = True
+        tconfig.Output = "cache_" + user + ".csv"
+        if user in data and data[user]:
+            tconfig.Since = data[user]
+
+        try:
+            twint.run.Search(tconfig)
+        except Exception as e:
+            # print(e)
+            print("Issue with Twitter scrapping, retry later")
+
+        try: 
+            with open("cache_" + user + ".csv", encoding="utf-8") as f:
+                cache = csv.reader(f)
+
+                next(cache)
+                c = next(cache)
+                data[user] = c[3] + " " + c[4]
+        except Exception as e:
+            # print(e)
+            print("Nothing found, creating blank csv.")
+            Path("cache_" + user + ".csv").touch()
+
+    with open("config.json", mode="w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+def download_cache(delete=True):
+    users = []
+    # users.txt : contains twitter usernames, one per line.
+    with open("users.txt", encoding="utf-8") as f:
+        for line in f.read().splitlines():
+            if line.startswith("# "): continue
+            users.append(line)
+
+    for user in users:
+        print(user)
+
+        options = {
+            'outtmpl': "images/" + user + "/" + '%(id)s.%(ext)s'
+        }
+
+        correct = True
+
         with open("cache_" + user + ".csv", encoding="utf-8") as f:
+            if len(f.read()) == 0:
+                correct = False
+
+        if not correct:
+            print("Blank csv, skipping.")
+            os.remove("cache_" + user + ".csv")
+            continue
+        
+        with open("cache_" + user + ".csv", encoding="utf-8") as f:
+
             cache = csv.reader(f)
 
             next(cache)
-            c = next(cache)
-            data[user] = c[3] + " " + c[4]
-    except Exception as e:
-        # print(e)
-        print("Nothing found, creating blank csv.")
-        Path("cache_" + user + ".csv").touch()
 
-with open("config.json", mode="w", encoding="utf-8") as f:
-    json.dump(data, f)
+            for row in cache:
+                photos = ast.literal_eval(row[14])
 
+                for photo in photos:
+                    name = os.path.basename(photo)
+                    if os.path.exists("images/" + user + "/" + name):
+                        if VERBOSE: print("Skipping " + photo)
+                        continue
 
-for user in users:
-    print(user)
+                    if VERBOSE: print("Downloading " + photo + "...")
+                    r = requests.get(photo)
 
-    options = {
-        'outtmpl': "static/" + user + "/" + '%(id)s.%(ext)s'
-    }
+                    open("images/" + user + "/" + name, "wb").write(r.content)
 
-    correct = True
+                url, thumb = row[20], row[24]
+                if "video_thumb" in thumb:
+                    try:
+                        with youtube_dl.YoutubeDL(options) as ydl:
+                            ydl.download([url])
+                    except Exception:
+                        with open("errors", mode="a", encoding="utf-8") as f:
+                            f.write(f"{user} {url}\n")
 
-    with open("cache_" + user + ".csv", encoding="utf-8") as f:
-        if len(f.read()) == 0:
-            correct = False
+        if delete:
+            os.remove("cache_" + user + ".csv")
 
-    if not correct:
-        print("Blank csv, skipping.")
-        os.remove("cache_" + user + ".csv")
-        continue
-    
-    with open("cache_" + user + ".csv", encoding="utf-8") as f:
-
-        cache = csv.reader(f)
-
-        next(cache)
-
-        for row in cache:
-            photos = ast.literal_eval(row[14])
-
-            for photo in photos:
-                name = os.path.basename(photo)
-                if os.path.exists("static/" + user + "/" + name):
-                    if VERBOSE: print("Skipping " + photo)
-                    continue
-
-                if VERBOSE: print("Downloading " + photo + "...")
-                r = requests.get(photo)
-
-                open("static/" + user + "/" + name, "wb").write(r.content)
-
-            url, thumb = row[20], row[24]
-            if "video_thumb" in thumb:
-                try:
-                    with youtube_dl.YoutubeDL(options) as ydl:
-                        ydl.download([url])
-                except Exception:
-                    with open("errors", mode="a", encoding="utf-8") as f:
-                        f.write(f"{user} {url}\n")
-
-    os.remove("cache_" + user + ".csv")
+create_cache()
+download_cache()
